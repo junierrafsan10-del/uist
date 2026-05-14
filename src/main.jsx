424d5c8dbd1dc10fa9309936f1a6f6d2d2a43414
UCEP Developer <dev@ucep.edu.bd>
2026-05-15 04:17:34 +0600

    Fix router basename for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/institute-management">
+    <BrowserRouter basename="/">
       <AuthProvider>
         <App />
       </AuthProvider>
